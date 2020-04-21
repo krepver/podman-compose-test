9a3febc5d20a320f8cc4bcba911069f930d70e7a
Aleksei Aleshin <alalexey@gmail.com>
2020-04-21 11:50:04 +0300

    Рабочий вариант

--- app/test.php
+++ app/test.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+<body>
 <?php
-echo "PHP Ok".PHP_EOL;
+echo "<h1>PHP Ok</h1>" . PHP_EOL;
+
+$f = fopen("./test.txt", 'w');
+if ($f) {
+    fwrite($f, "test test test" . PHP_EOL);
+    fclose($f);
+} else {
+    echo "<p>Error opening!</p>" . PHP_EOL;
+}
+
+?>
+</body>
+</html>
+
 
